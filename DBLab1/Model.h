5eb44487a41bb437ece40d25a9fc96585d5c3b80
Yurii Kovalchuk <urakovalchuk3535@gmail.com>
2024-06-03 19:03:10 +0300

    1

--- DBLab1/Model.h
+++ DBLab1/Model.h
@@ -1,6 +1,30 @@
 #pragma once
 
 namespace Model {
-	#include "Product.h"
-	#include "Category.h"
+	struct Product
+	{
+		long Id;
+		char Name[50];
+		long Price;
+		long CategoryId;
+		char Description[50];
+
+		static constexpr size_t size
+			= sizeof(Id)
+			+ sizeof(Name)
+			+ sizeof(Price)
+			+ sizeof(CategoryId)
+			+ sizeof(Description);
+	};
+
+	struct Category
+	{
+		long Id;
+		char Name[50];
+
+		static constexpr size_t size
+			= sizeof(Id)
+			+ sizeof(Name);
+	};
+
 }
